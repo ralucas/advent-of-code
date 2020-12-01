eddb723232772e1394cc3489905f52d4da79fd4c
R.A. Lucas <richard.lucas@alteryx.com>
2020-12-01 15:15:33 -0700

    initial commit

---
+++ cmd/expensereport/expensereport_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	util "github.com/ralucas/advent-of-code/internal"
+	"math/rand"
+	"testing"
+	"time"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestTwoSum(t *testing.T) {
+	data := prepareData("../../assets/1/input.txt")
+	dlen := len(data)
+
+	x, y := 0, 0
+
+	for i := 0; i < 100000; i++ {
+		for {
+			time.Sleep(time.Nanosecond * 100)
+			rand.Seed(time.Now().UnixNano())
+			x, y = rand.Intn(dlen), rand.Intn(dlen)
+			if x != y {
+				break
+			}
+		}
+		targ := data[x] + data[y]
+		a, b := twoSum(data, targ)
+		assert.Equal(t, targ, a+b, fmt.Sprintf("got %d + %d, expected %d + %d = %d", a, b, x, y, targ))
+	}
+}
+
+func TestTwoSumSorted(t *testing.T) {
+	data := prepareData("../../assets/1/input.txt")
+	sData := util.QSort(data)
+	dlen := len(data)
+
+	x, y := 0, 0
+
+	for i := 0; i < 100000; i++ {
+		for {
+			time.Sleep(time.Nanosecond * 100)
+			rand.Seed(time.Now().UnixNano())
+			x, y = rand.Intn(dlen), rand.Intn(dlen)
+			if x != y {
+				break
+			}
+		}
+		targ := data[x] + data[y]
+		a, b := twoSumSorted(sData, targ)
+		assert.Equal(t, targ, a+b, fmt.Sprintf("got %d + %d, expected %d + %d = %d", a, b, x, y, targ))
+	}
+}
+
+func TestThreeSum(t *testing.T) {
+	data := prepareData("../../assets/1/input.txt")
+	sData := util.QSort(data)
+	dlen := len(data)
+	x, y, z := 0, 0, 0
+
+	for i := 0; i < 100000; i++ {
+		for {
+			time.Sleep(time.Nanosecond)
+			rand.Seed(time.Now().UnixNano())
+			x, y, z = rand.Intn(dlen), rand.Intn(dlen), rand.Intn(dlen)
+			if x != y && y != z && x != z {
+				break
+			}
+		}
+		targ := data[x] + data[y] + data[z]
+		a, b, c := threeSum(sData, targ)
+		assert.Equal(t, targ, a+b+c, fmt.Sprintf(
+			"got %d, %d, %d, expected: %d + %d + %d = %d",
+			a, b, c, data[x], data[y], data[z], targ))
+	}
+}
