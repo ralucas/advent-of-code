696d3b620ed9fb02981105b2d2f79f2b91a0d918
R.A. Lucas <richard.lucas@alteryx.com>
2020-12-09 11:00:49 -0700

    add parallelism to 9

--- pkg/9/day9.go
+++ pkg/9/day9.go
@@ -22,6 +22,7 @@ func PrepareData(filepath string) []int {
 func FindFirstNonSum(nums []int, preamble int) (int, int) {
 	i := 0
 	nlen := len(nums)
+
 	for j := preamble + 1; j < nlen; j++ {
 		a, b := day1.TwoSum(nums[i:j], nums[j])
 		if a == -1 && b == -1 {
@@ -36,25 +37,40 @@ func FindFirstNonSum(nums []int, preamble int) (int, int) {
 func ContiguousSumSet(nums []int, target int) []int {
 	nlen := len(nums)
 
+	ch := make(chan []int)
+
 	for i := 0; i < nlen; i++ {
-		sumSet := make([]int, 0)
-		curSum := nums[i]
-		if curSum > target {
-			continue
-		}
-		sumSet = append(sumSet, nums[i])
-		for j := i + 1; j < nlen; j++ {
-			curSum += nums[j]
-			sumSet = append(sumSet, nums[j])
-			if curSum == target {
-				return sumSet
-			}
+		go func(start int) {
+			sumSet := make([]int, 0)
+			curSum := nums[start]
 			if curSum > target {
-				break
+				ch <- nil
+				return
 			}
+			sumSet = append(sumSet, nums[start])
+			for j := start + 1; j < nlen; j++ {
+				curSum += nums[j]
+				sumSet = append(sumSet, nums[j])
+				if curSum == target {
+					ch <- sumSet
+					return
+				}
+				if curSum > target {
+					ch <- nil
+					return
+				}
+			}
+		}(i)
+	}
+
+	for i := 0; i < nlen; i++ {
+		ss := <-ch
+		if ss != nil {
+			return ss
 		}
 	}
 
+	defer close(ch)
 	return nil
 }
 
